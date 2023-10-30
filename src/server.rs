7f9643765bfd27224f9bc506c7b0c1166e90e7b5
Alek Michelson <am7590@rit.edu>
2023-10-29 22:46:47 -0400

    Pusher works

--- src/server.rs
+++ src/server.rs
@@ -4,6 +4,7 @@ use sqlx_postgres::{PgPool, PgPoolOptions};
 use std::borrow::BorrowMut;
 use std::pin::Pin;
 use std::sync::Arc;
+use std::time::{SystemTime, UNIX_EPOCH};
 use tokio::sync::{mpsc, Mutex};
 use tokio_stream::wrappers::UnboundedReceiverStream;
 use tonic::{Request, Response, Status};
@@ -64,7 +65,8 @@ impl PlantService for StorePlant {
         let last_identification = information.last_identification;
 
         let result = sqlx::query!(
-            "INSERT INTO plants (sku, name, last_watered, last_health_check, last_identification) VALUES ($1, $2, $3, $4, $5)
+            "
+            INSERT INTO plants (sku, name, last_watered, last_health_check, last_identification) VALUES ($1, $2, $3, $4, $5)
             ",
             sku,
             name,
@@ -78,7 +80,7 @@ impl PlantService for StorePlant {
         match result {
             Ok(_) => {
                 println!("Add item");
-                let _ = push::apns::run().await;
+                // let _ = push::apns::run().await;
                 Ok(Response::new(PlantResponse {
                     status: "success".into(),
                 }))
@@ -186,15 +188,20 @@ impl PlantService for StorePlant {
         &self,
         _request: Request<()>,
     ) -> Result<Response<ListOfPlants>, Status> {
-        let current_date = chrono::Utc::now().date();
-        let int64_current_date = current_date.and_hms(0, 0, 0).timestamp();
-    
+        let int64_current_date = SystemTime::now();
+        let unix_timestamp = match int64_current_date.duration_since(UNIX_EPOCH) {
+            Ok(duration) => duration.as_secs() as i64,
+            Err(_) => -1, // Handle errors here if necessary
+        };
+
+        println!("get watered");
+
         let result = sqlx::query!(
-            "SELECT * FROM plants WHERE last_watered < $1",
-            int64_current_date,
+            "SELECT * FROM plants WHERE last_watered <= $1",
+            unix_timestamp,
         )
         .fetch_all(&self.pool)
-        .await;
+        .await;        
     
         match result {
             Ok(rows) => {
@@ -206,6 +213,8 @@ impl PlantService for StorePlant {
                         let last_watered = row.last_watered; //.ok_or(Status::internal("Missing last_watered"))?;
                         let last_health_check = row.last_health_check; //.ok_or(Status::internal("Missing last_health_check"))?;
                         let last_identification = row.last_identification; //.ok_or(Status::internal("Missing last_identification"))?;
+
+                        println!("{} is getting a push", sku);
 
                         Ok::<_, Status>(Plant {
                             identifier: Some(PlantIdentifier { sku }),
